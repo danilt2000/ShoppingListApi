fc48602dbc2f2b3ceba7f5809690b5c12eb5147e
Danil Tkachenko <d_tkachenko@utb.cz>
2022-06-20 19:55:57 +0200

    Add skyfoldItem

--- ShoppingListApi/Program.cs
+++ ShoppingListApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingListApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ShoppingListContext>(opt => opt.UseInMemoryDatabase("ShoppingList"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
